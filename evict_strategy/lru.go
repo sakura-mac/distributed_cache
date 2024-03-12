26e1ccc772cc21517e7090b23afa9be6c2877408
Khalil Chen <56813759+sakura-mac@users.noreply.github.com>
2024-03-12 22:39:49 +0800

    lru strategy for cache eviction

---
+++ evict_strategy/lru.go
@@ -0,0 +1,85 @@
+package lru
+
+import (
+	"container/list"
+	"sync"
+)
+
+type Cache struct {
+	maxBytes int64
+	nbytes   int64
+	lock     sync.Mutex
+	ll       *list.List
+	cache    map[string]*list.Element
+	// callback function
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func NewLRU(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+
+// Remove the tail element
+func (c *Cache) RemoveOldest() {
+	// c.lock.Lock()
+	// defer c.lock.Unlock()
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// Len the number of cache entries
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
